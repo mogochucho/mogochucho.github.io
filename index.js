572d9fa6b8a5b8b47d191b4fe83964bbde12cf85
Matías Esmoris <esmoris.matias@gmail.com>
2020-09-03 04:01:58 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const getLocal = async()=>document.querySelector('.select-file > .script-local').files[0].text()
-const getRemote = async()=>fetch(document.querySelector('.select-file > .script-remote').value).then(x=>x.text())
+const getLocal = async()=>document
+  .querySelector('.select-file > .script-local')
+  .files[0].text();
+const getRemote = async()=>fetch(
+    document.querySelector('.select-file > .script-remote')
+      .value
+  ).then(x=>x.text());
 
 window.addEventListener('load',()=>{
-	document.querySelector('.select-file > .script-remote').addEventListener('change',async()=>localStorage.init = await getRemote())
-	const scriptLocal = document.querySelector('.select-file > .script-local')
-	document.querySelector('.select-file > .button-get-local').addEventListener('click',()=>scriptLocal.click())
-	document.querySelector('.select-file > .script-local').addEventListener('change',async()=>localStorage.init = await getLocal())
-	eval(localStorage.init || '')
-})
+	document.querySelector('.select-file > .script-remote')
+	  .addEventListener(
+	    'change',
+	    async()=>localStorage.init = await getRemote()
+	  );
+	const scriptLocal = document
+	  .querySelector('.select-file > .script-local');
+	document.querySelector('.select-file > .button-get-local')
+	  .addEventListener('click',()=>scriptLocal.click());
+	document.querySelector('.select-file > .script-local')
+	  .addEventListener(
+	    'change',
+	    async()=>localStorage.init = await getLocal()
+	  );
+	eval(localStorage.init || '');
+});
